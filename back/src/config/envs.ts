a05f526f4dc241d531140a80c332741dcefc2a5e
David Natan <rdpalominop@ucvvirtual.edu.pe>
2025-02-10 16:47:57 +0100

    envs add

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -1,5 +1,5 @@
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 
 // export const PORT: number = Number(process.env.PORT) || 3000;
 // export const DB_NAME: string = process.env.DB_NAME || "proyecto_m4_front";
@@ -7,4 +7,4 @@
 // export const DB_PASSWORD: string = process.env.DB_PASSWORD || "admin";
 // export const DB_HOST: string = process.env.DB_HOST || "localhost";
 // export const DB_PORT: number = Number(process.env.DB_PORT) || 5432;
-// export const JWT_SECRET: string = process.env.JWT_SECRET || "secret";
+export const JWT_SECRET: string = process.env.JWT_SECRET || "secret";
